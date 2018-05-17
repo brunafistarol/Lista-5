6ab59bde12e134f81d1c282423cdab829a58391b
Henrique Muniz <hnmuniza@gmail.com>
2018-05-17 15:09:55 -0300

    Update tests.lisp

--- tests.lisp
+++ tests.lisp
@@ -7,3 +7,20 @@
 (defun test-lista-5 ()
   (run! 'all-tests))
 
+(test test-question-4.1-a)
+
+(test test-question-4.1-b)
+
+(test test-question-4.2-a)
+
+(test test-question-4.2-b)
+
+(test test-question-4.5)
+
+(test test-question-4.11)
+
+(test test-question-4.13-a)
+
+(test test-question-4.13-b)
+
+(test test-question-4.18)
